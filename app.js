7c483ccc6cfaff43a5cef717d269299187c3e8b9
Ilya200918 <95967188+Ilya200918@users.noreply.github.com>
2023-02-04 20:15:00 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function first() {
+    alert(`Будет ${2+2}`)
+    }
+    function second() {
+    let age = prompt('Введи своё имя: ')
+    }
+    function third() {
+    document.getElementById('zp').innerHTML = '10000$';
+    }
+    
